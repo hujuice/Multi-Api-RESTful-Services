dfe650423c81bdb6d7f019d90fe773cb7ff08df0
Sergio Vaccaro <hujuice@inservibile.org>
2012-03-23 10:48:09 +0100

    Better repsentation and example

--- example/htdocs/index.php
+++ example/htdocs/index.php
@@ -1,11 +1,12 @@
 <?php
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(__DIR__ . '/../resources'),
+    realpath(__DIR__ . '/../library'),
     get_include_path(),
 )));
 
-require_once(realpath(__DIR__ . '/../..') . '/library/Restful/Server.php');
-$restJson = new Restful_Server('../config.ini');
+require_once('restful/server.php');
+$rest = new Restful\Server('../config.ini');
 
 // GO GO GO!!!
-$restJson->run();
+$rest->run();
+
